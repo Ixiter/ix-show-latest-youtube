bff07cf8632af834ac917524683080bbe587a196
ixiter <ixiter@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-19 02:13:52 +0000

    tagged version 1.0 created
    git-svn-id: https://plugins.svn.wordpress.org/ix-show-latest-youtube/tags/1.0@626793 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ loader.php
@@ -0,0 +1,12 @@
+<?php
+/*
+  Plugin Name: IX Show Latest YouTube
+  Description: Provides a shortcode and a template tag to embed the lates video or current live stream of a specified YouTube channel.
+  Author: Ixiter IT - Peter Liebetrau <peter.liebetrau@ixiter.com>
+  Version: 1.0
+  Plugin-Uri: http://ixiter.it/en/ix-plugins/ix-show-latest-youtube/
+  Author-Uri: https://plus.google.com/u/0/102408750978338972864/about
+ */
+defined( 'ABSPATH' ) or die( 'Fail' );
+
+require_once dirname(__FILE__).'/plugin/Ix_ShowLatestYt.php';
