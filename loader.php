9eaf8b2881cbfc48fe62ad969dc80cce0ba1197a
Daniel J. Lewis <Daniel@DJosephDesign.com>
2014-06-19 14:57:04 -0400

    Spacing in plugin

--- loader.php
+++ loader.php
@@ -1,12 +1,12 @@
 <?php
 /*
- Plugin Name: IX Show Latest YouTube
- Description: Provides a shortcode and a template tag to embed the lates video or current live stream of a specified YouTube channel.
- Author: Ixiter IT - Peter Liebetrau <peter.liebetrau@ixiter.com>
- Version: 2.3.1
- Plugin-Uri: http://ixiter.it/ix-show-latest-youtube
- Author-Uri: https://plus.google.com/u/0/102408750978338972864/about
- */
+Plugin Name: IX Show Latest YouTube
+Description: Provides a shortcode and a template tag to embed the lates video or current live stream of a specified YouTube channel.
+Author: Ixiter IT - Peter Liebetrau <peter.liebetrau@ixiter.com>
+Version: 2.3.1
+Plugin-Uri: http://ixiter.it/ix-show-latest-youtube
+Author-Uri: https://plus.google.com/u/0/102408750978338972864/about
+*/
 defined( 'ABSPATH' ) or die( 'Fail' );
 
 require_once dirname(__FILE__).'/plugin/Ix_ShowLatestYt.php';
